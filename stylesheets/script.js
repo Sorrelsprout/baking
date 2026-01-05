60999c95911ba31030e7f52fa10b263d797a6d0b
Sorrel <sorrelsprouts@gmail.com>
2026-01-04 22:01:35 -0800

    Updated entries

--- stylesheets/script.js
+++ stylesheets/script.js
@@ -86,27 +86,13 @@ $(document).ready(function(){
         setPullup(); /* Project Content Setup */
     });
 
-    $("#cardwelling").click(function() {
-        $("#pullupContent .fullDescription").load("pages/cardwelling.html"); 
-        $("#pullupContent .hero").css({ 
-            // "background":"url(images/cardwelling/banner-cardwelling.jpg)", 
-            "background":"url(https://images.unsplash.com/photo-1589830517302-44c51f875686?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2342&q=80)", 
-            "background-position":"50% 50%",
-            "background-size":"cover",
-            "background-repeat":"no-repeat" 
-        });
-        $("#pullupContent .hero h1").html("Car Dwelling"); /* Project Name Setup */
-        $("#pullupContent .hero p").html("");
-        setPullup(); /* Project Content Setup */
-    });
-
     function setPullup() {
         $("#pullupContent").addClass("show");
         $("#pullup").addClass("show"); 
     }
 
     $("#pullupToggle").click(function() { hidePullup() });
-    $("#travel").click(function() { hidePullup() });
+    // $("#travel").click(function() { hidePullup() });
     $("#logo").click(function() { hidePullup() });
     function hidePullup(){
         $("#pullup").removeClass("show").scrollTop(0);
